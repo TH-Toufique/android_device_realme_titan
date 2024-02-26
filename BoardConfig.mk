ce5008c8082871c2e33def3fc355194ed62c32d2
TH-Toufique <mrripp3r96@gmail.com>
2024-02-26 20:31:01 +0600

    Define Bootloader and platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6877
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6877
+
 
 # Inherit the proprietary files
 include vendor/realme/RMX3686/BoardConfigVendor.mk
