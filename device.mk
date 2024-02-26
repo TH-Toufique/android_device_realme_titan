06ab0faf86fcb6bc3367b087567adcccf5682778
TH-Toufique <mrripp3r96@gmail.com>
2024-02-27 01:33:01 +0600

    Rename `libwifi-hal.so` to prevent conflict with AOSP

--- device.mk
+++ device.mk
@@ -286,7 +286,7 @@ PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.control@1.1.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy
+    android.hardware.wifi@1.0-service-lazy.RMX3686
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/RMX3686/RMX3686-vendor.mk)
