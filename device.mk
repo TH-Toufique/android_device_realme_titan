922e6f27206eadc6dd2e18ec4f3101c89f15c26e
TH-Toufique <mrripp3r96@gmail.com>
2024-02-28 03:20:11 +0600

    Use ndk instead of ndk_platform
     as ndk_platform will deprecated soon

--- device.mk
+++ device.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
 
 # Biometrics
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.common-V1-ndk_platform.vendor \
     android.hardware.biometrics.fingerprint@2.3-service.RMX3686
 
 # Boot control
